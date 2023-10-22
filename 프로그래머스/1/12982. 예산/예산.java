4f0c35ea917b9c292165be79b78728aeb805319e
SeoJin <78786486+bsjin1122@users.noreply.github.com>
2023-10-23 08:25:02 +0900

    [level 1] Title: 예산, Time: 0.41 ms, Memory: 73.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -0,0 +1,19 @@
+/*예산*/
+import java.util.*;
+
+class Solution {
+    public int solution(int[] d, int budget) {
+        int answer = 0;
+        int change = budget;
+        Arrays.sort(d);
+        // 힌트얻음
+        for(int i=0; i < d.length; i++) { 
+            if(budget < d[i]) {
+                break;
+            }
+            budget -= d[i];
+            answer++;
+        }
+        return answer;
+    }
+}
