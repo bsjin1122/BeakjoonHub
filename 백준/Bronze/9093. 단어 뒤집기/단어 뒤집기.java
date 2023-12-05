c75c90c30efe319d0d990fc0a2a1a3c66d7801ed
SeoJin <78786486+bsjin1122@users.noreply.github.com>
2023-12-06 00:23:28 +0900

    [Bronze I] Title: 단어 뒤집기, Time: 484 ms, Memory: 48264 KB -BaekjoonHub

---
+++ 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+class Main { // 단어 뒤집기
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+
+        for(int i = 0; i < n; i++){
+            String str = br.readLine();
+            String reversedString = reverseWords(str);
+            bw.write(reversedString +"\n");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    public static String reverseWords(String str){
+        String[] splitArray = str.split(" ");
+        StringBuilder reversedString = new StringBuilder();
+
+        for(String word: splitArray){
+            StringBuilder reversedWord = new StringBuilder(word).reverse();
+            reversedString.append(reversedWord).append(" "); // 뒤집은 단어를 결과 문자열에 추가
+        }
+        return reversedString.toString().trim(); // 문자열 앞뒤 공백 제거
+    }
+
+}
