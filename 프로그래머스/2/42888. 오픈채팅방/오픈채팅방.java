97f35707818b774b7db2d478191c896357e627e0
SeoJin <78786486+bsjin1122@users.noreply.github.com>
2024-07-28 19:53:20 +0900

    [level 2] Title: 오픈채팅방, Time: 169.93 ms, Memory: 188 MB -BaekjoonHub

---
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
+class Solution {
+    public String[] solution(String[] record) {
+        // Enter/Leave 메시지를 저장할 해시맵 생성
+		HashMap<String, String> msg = new HashMap<>();
+		msg.put("Enter", "님이 들어왔습니다.");
+		msg.put("Leave", "님이 나갔습니다.");
+
+		HashMap<String, String> uid = new HashMap<>();
+		// record의 줄을 하나씩 처리
+		for(String s : record){
+			String[] cmd = s.split(" ");
+			if(cmd.length == 3){ // Enter 또는 Change인 경우
+				uid.put(cmd[1], cmd[2]);
+			}
+		}
+		// 답을 저장할 answer List 생성
+		ArrayList<String> answer = new ArrayList<>();
+
+		// record의 각 줄을 하나씩 처리
+		for(String s: record){
+			String[] cmd = s.split(" ");
+			// 각 상태에 맞는 메시지를 answer 에 저장
+			if(msg.containsKey(cmd[0])){
+				answer.add(uid.get(cmd[1]) + msg.get(cmd[0])); // 유저 아이디, 닉네임
+			}
+		}
+        return answer.toArray(new String[0]);
+    }
+}
